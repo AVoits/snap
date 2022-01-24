c45ee3953925f6a7b42a7cc9c6ab81d3846d4529
Анастасия Войценя <vojtsenyaas@it2g.ru>
2022-01-24 15:37:22 +0300

    изменение модели

--- curve/model.ts
+++ curve/model.ts
@@ -1,6 +1,6 @@
-import {Coordinate} from "../tracks/tracks.model";
 import {CONTROL_DISTANCE} from "../tracks/tracks.const";
 import {v4 as uuidv4} from 'uuid';
+import {Coordinate, CurveData} from "./curve.model";
 
 
 export enum PointType {
@@ -24,6 +24,8 @@ export class Point {
 
     constructor(params: PointParams) {
         const {coordinate, arcID, type} = params
+        // console.log(params)
+
         this.x = coordinate.x
         this.y = coordinate.y
         this.arcID = arcID
@@ -37,8 +39,7 @@ export class Point {
 }
 
 export interface ArcParams {
-    start: Coordinate,
-    end: Coordinate,
+    data: CurveData,
     arcID: number
 }
 
@@ -52,14 +53,24 @@ export class Arc {
     arcID: number
     arcColor: string
 
-    constructor(params: ArcParams) {
-        const {start, end, arcID} = params
-        this.arcID = arcID
-        this.anchorStart = new Point({coordinate: start, type: PointType.anchorStart, arcID})
-        this.anchorEnd = new Point({coordinate: end, type: PointType.anchorEnd, arcID})
 
-        this.controlStart = new Point({coordinate: this.makeControlStart(start), type: PointType.controlStart, arcID})
-        this.controlEnd = new Point({coordinate: this.makeControlEnd(end), type: PointType.controlEnd, arcID})
+    constructor(params: ArcParams) {
+        this.createByCurveData(params)
+    }
+
+    private createByCurveData(params: ArcParams) {
+        const {data, arcID} = params
+
+        this.arcID = arcID
+
+        this.anchorStart = new Point({coordinate: data.start, type: PointType.anchorStart, arcID})
+        this.anchorEnd = new Point({coordinate: data.end, type: PointType.anchorEnd, arcID})
+
+        const startControlCoordinate = data.cStart ? data.cStart : this.makeControlStart(data.start)
+        this.controlStart = new Point({coordinate: startControlCoordinate, type: PointType.controlStart, arcID})
+
+        const endControlCoordinate = data.cEnd ? data.cEnd : this.makeControlEnd(data.end)
+        this.controlEnd = new Point({coordinate: endControlCoordinate, type: PointType.controlEnd, arcID})
     }
 
     private makeControlStart(point: Coordinate) {
@@ -84,15 +95,10 @@ export class Arc {
 export class Curve {
     arcs: Arc[] = []
 
-    constructor(data: Coordinate[]) {
-        for (let i = 0; i < data.length - 1; i++) {
-
-            const start = data[i]
-            const end = data[i + 1]
-
-            const arc = new Arc({start, end, arcID: uuidv4()})
-            this.arcs.push(arc)
-        }
+    constructor(data: CurveData[]) {
+        this.arcs = data.map(item => {
+            return new Arc({data: item, arcID: uuidv4()})
+        })
     }
 
     public moveAnchor(point: Point, dx: number, dy: number, x: number, y: number, event: MouseEvent) {
@@ -153,14 +159,18 @@ export class Curve {
 
         const firstArc = new Arc({
             arcID: uuidFirst,
-            start: arc.anchorStart,
-            end: firstPoint
-            })
+            data: {
+                start: arc.anchorStart,
+                end: firstPoint
+            }
+        })
 
         const secondArc = new Arc({
             arcID: uuidSecond,
-            start: secondPoint,
-            end: arc.anchorEnd
+            data: {
+                start: secondPoint,
+                end: arc.anchorEnd
+            }
         })
 
         return [firstArc, secondArc]
@@ -171,7 +181,7 @@ export class Curve {
         const currentArcIndex = this.arcs.findIndex((item) => arc.arcID === item.arcID)
         const newArcs = this.splitArc(arc, x, y)
 
-        if(currentArcIndex !== -1){
+        if (currentArcIndex !== -1) {
             this.arcs.splice(currentArcIndex, 1, ...newArcs)
         }
     }
@@ -179,8 +189,10 @@ export class Curve {
     private joinArcs(start: Arc, end: Arc): Arc {
         return new Arc({
             arcID: uuidv4(),
-            start: start.anchorStart,
-            end: end.anchorEnd
+            data: {
+                start: start.anchorStart,
+                end: end.anchorEnd
+            }
         })
     }
 
@@ -190,11 +202,11 @@ export class Curve {
             return item.arcID === point.arcID
         })
 
-        if(this.arcs.length === 1){
+        if (this.arcs.length === 1) {
             return
         }
 
-        if(point.type === PointType.anchorEnd){
+        if (point.type === PointType.anchorEnd) {
             this.arcs.splice(-1, 1)
 
             return;
@@ -203,7 +215,7 @@ export class Curve {
         const currentArc = this.arcs[indexArc]
         const prevArc = this.arcs[indexArc - 1]
 
-        if(!prevArc){
+        if (!prevArc) {
             this.arcs.splice(0, 1)
             return;
         }
--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import {Coordinate} from "./tracks/tracks.model";
 import {Controller} from "./curve/controller";
+import {CurveData} from "./curve/curve.model";
+import Snap = require("snapsvg");
 
 const example: Coordinate[] = [
     {x: 100, y: 350},
@@ -9,5 +11,267 @@ const example: Coordinate[] = [
     {x: 800, y: 350},
 ]
 
-const controller = new Controller(example)
+// @ts-ignore
+const test = Array.from(document.querySelectorAll('path')).map(i => i.getAttribute('d'))
+const mapPathToCurveData = (arr: string[]): CurveData[] => {
+    return  arr.map((item) => {
+        const data = Snap.parsePathString(item)
+        // ['M', 94, 383]
+        // ['C', 116, 274, 229, 256, 173, 189]
+        const [M, C] = data
+
+        const curvedata: CurveData = {
+            start: {x: M[1], y: M[2]},
+            end: {x: C[5], y: C[6]},
+            cStart: {x: C[1], y: C[2]},
+            cEnd: {x: C[3], y: C[4]},
+        }
+
+        return curvedata
+    })
+}
+const initData__ = mapPathToCurveData(test)
+
+
+const initData = [
+    {
+        "start": {
+            "x": 94,
+            "y": 383
+        },
+        "end": {
+            "x": 173,
+            "y": 189
+        },
+        "cStart": {
+            "x": 116,
+            "y": 274
+        },
+        "cEnd": {
+            "x": 229,
+            "y": 256
+        }
+    },
+    {
+        "start": {
+            "x": 173,
+            "y": 189
+        },
+        "end": {
+            "x": 145,
+            "y": 456
+        },
+        "cStart": {
+            "x": 95,
+            "y": 128
+        },
+        "cEnd": {
+            "x": 215,
+            "y": 344
+        }
+    },
+    {
+        "start": {
+            "x": 145,
+            "y": 456
+        },
+        "end": {
+            "x": 232,
+            "y": 467
+        },
+        "cStart": {
+            "x": 184,
+            "y": 162
+        },
+        "cEnd": {
+            "x": 259,
+            "y": 388
+        }
+    },
+    {
+        "start": {
+            "x": 232,
+            "y": 467
+        },
+        "end": {
+            "x": 321,
+            "y": 318
+        },
+        "cStart": {
+            "x": 285,
+            "y": 389
+        },
+        "cEnd": {
+            "x": 388,
+            "y": 351
+        }
+    },
+    {
+        "start": {
+            "x": 321,
+            "y": 318
+        },
+        "end": {
+            "x": 351,
+            "y": 430
+        },
+        "cStart": {
+            "x": 253,
+            "y": 286
+        },
+        "cEnd": {
+            "x": 264,
+            "y": 538
+        }
+    },
+    {
+        "start": {
+            "x": 351,
+            "y": 430
+        },
+        "end": {
+            "x": 409,
+            "y": 188
+        },
+        "cStart": {
+            "x": 407,
+            "y": 350
+        },
+        "cEnd": {
+            "x": 482,
+            "y": 174
+        }
+    },
+    {
+        "start": {
+            "x": 409,
+            "y": 188
+        },
+        "end": {
+            "x": 416,
+            "y": 450
+        },
+        "cStart": {
+            "x": 363,
+            "y": 197
+        },
+        "cEnd": {
+            "x": 362,
+            "y": 392
+        }
+    },
+    {
+        "start": {
+            "x": 416,
+            "y": 450
+        },
+        "end": {
+            "x": 503,
+            "y": 189
+        },
+        "cStart": {
+            "x": 448,
+            "y": 493
+        },
+        "cEnd": {
+            "x": 557,
+            "y": 240
+        }
+    },
+    {
+        "start": {
+            "x": 503,
+            "y": 189
+        },
+        "end": {
+            "x": 491,
+            "y": 448
+        },
+        "cStart": {
+            "x": 440,
+            "y": 192
+        },
+        "cEnd": {
+            "x": 454,
+            "y": 377
+        }
+    },
+    {
+        "start": {
+            "x": 491,
+            "y": 448
+        },
+        "end": {
+            "x": 552,
+            "y": 388
+        },
+        "cStart": {
+            "x": 503,
+            "y": 459
+        },
+        "cEnd": {
+            "x": 544,
+            "y": 494
+        }
+    },
+    {
+        "start": {
+            "x": 552,
+            "y": 388
+        },
+        "end": {
+            "x": 621,
+            "y": 413
+        },
+        "cStart": {
+            "x": 569,
+            "y": 511
+        },
+        "cEnd": {
+            "x": 614,
+            "y": 464
+        }
+    },
+    {
+        "start": {
+            "x": 621,
+            "y": 413
+        },
+        "end": {
+            "x": 552,
+            "y": 388
+        },
+        "cStart": {
+            "x": 639,
+            "y": 293
+        },
+        "cEnd": {
+            "x": 547,
+            "y": 287
+        }
+    },
+    {
+        "start": {
+            "x": 552,
+            "y": 388
+        },
+        "end": {
+            "x": 721,
+            "y": 312
+        },
+        "cStart": {
+            "x": 600,
+            "y": 340
+        },
+        "cEnd": {
+            "x": 726,
+            "y": 413
+        }
+    }
+]
+
+
+const controller = new Controller(initData)
 controller.draw()
+
+
